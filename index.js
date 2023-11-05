c059069f9a96f29ae717bb59bdedd54894c9a061
ishaECE <ishadua98@gmail.com>
2023-11-06 01:42:38 +0530

    criket_score_count

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
-function increment() {
+function increment1() {
     count += 1
+    countEl.textContent = count
+}
+
+function increment4() {
+    count += 4
+    countEl.textContent = count
+}
+
+function increment6() {
+    count += 6
     countEl.textContent = count
 }
 
